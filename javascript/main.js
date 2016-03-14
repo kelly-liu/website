30787d9f3469cfcf989fab7148e370dc001ea0bf
Kelly Liu <kellyliu@dhcp-18-189-23-175.dyn.MIT.EDU>
2016-03-14 02:49:47 -0400

    added resizing for About page, projects page, home page

--- javascript/main.js
+++ javascript/main.js
@@ -11,23 +11,24 @@ $('.project-item').click(function() {
 		var expander = $(this).children().eq(2); //gets expander class
 		// var open = $(this).children().eq(1).children(2);
 		
-		var margin = $(window).width() *.12;
+		var margin = ($(window).width() - $(".main").width())/2;
+		// console.log("window", $(window).width());
+		// console.log("main", $(".main").width());
+		// console.log("margin",margin);
 		expander.css("margin-left", 0);
 
 		if (expander.css("display") == "block") { //get rid of menus
-			// expander.css("display", "none");
-			var offsetAdjusted = expander.offset().left - margin;
-			expander.css("margin-left", -offsetAdjusted);
-			expander.width($(window).width()-2*margin);
+			// var offsetAdjusted = expander.offset().left - margin;
+			// expander.css("margin-left", -offsetAdjusted);
+			// expander.width($(window).width()-2*margin);
 			// expander.slideToggle("slide");
 			expander.css("display", "none");
-			// open.css("display", "none");
-			// caption.removeClass("open");
+
 		}
 		else {
 			$(".project-expander").css("display", "none");
 		// expander.slideToggle("slide");
-		console.log(expander);
+		// console.log(expander);
 		expander.css("display", "block");
 		// var margin = $(window).width() *.12;
 		var offsetAdjusted = expander.offset().left - margin;
